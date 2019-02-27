991c996fdb1284bd0406a6ab33a0f9e372c48628
John Oksasoglu <john.oksasoglu@gmail.com>
2019-02-26 22:03:43 -0800

    Add results for all posts up to time of this message

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 
 const threadUrl = 'https://www.doomworld.com/forum/topic/70830-post-your-doom-picture-part-2/';
 const startPage = 400;
-const endPage = 400;
+const endPage = 518;
 const delay = 1000 * 5;
 
 parseThread(threadUrl, startPage, endPage, delay)
 .then(results => {
     console.log(`script: finished parsing thread with results ${JSON.stringify(results)}`);
-    fs.writeFile('results.json', JSON.stringify(results, null, 4), 'utf8', () => {
-        console.log(`script: wrote results.json`);
+    const filename = `results_${startPage}-${endPage}.json`;
+    fs.writeFile(filename, JSON.stringify(results, null, 4), 'utf8', () => {
+        console.log(`script: wrote ${filename}`);
     });
 });
